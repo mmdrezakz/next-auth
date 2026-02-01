75f8ad6166132a3af7b25f3f3c6593079f0aed0a
Mohammadreza <mellboybeatprod@gmail.com>
2026-02-01 08:03:59 +0330

    fixed - errors google o-auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,21 +3,28 @@ import { signIn } from 'next-auth/react'
 import React, { useState } from 'react'
 
 export default function LoginPage() {
-  const [isLoading, setIsLoading] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
 
-  const handleLogin = async () => {
-    setIsLoading(true)
-    try {
-      await signIn("github", { 
-        callbackUrl: "/"  // بعد از لاگین به کجا بره
-      })
-      await signIn("google",{
-        callbackUrl: "/"  // بعد از لاگین به کجا بره
-      })
-    } catch (error) {
-      console.error("Login error:", error)
-      setIsLoading(false)
-    }
+  const handleGoogleLogin = async () => {
+
+    console.log("Google button clicked!") // این خط رو اضافه کن
+    await signIn("google", { 
+      callbackUrl: "/",
+      redirect: true 
+    })
+
+
+  }
+
+  const handleGitHubLogin = async () => {
+
+
+    console.log("GitHub button clicked!") // این خط رو اضافه کن
+    await signIn("github", { 
+      callbackUrl: "/",
+      redirect: true 
+    })
+
   }
 
   return (
@@ -26,14 +33,14 @@ export default function LoginPage() {
         <header className="mb-6 font-bold text-2xl text-center">LOGIN</header>
         <form>
           <button 
-            onClick={handleLogin}
-            disabled={isLoading}
+            onClick={handleGitHubLogin}
+            
             className="bg-black hover:bg-gray-800 disabled:opacity-50 px-6 py-3 rounded-lg w-full text-white"
           >
-            {isLoading ? "Redirecting to GitHub..." : "Login with GitHub"}
+            Login with GitHub
           </button>
                     <button 
-            onClick={() => signIn("google")}
+            onClick={handleGoogleLogin}
             className="flex justify-center items-center gap-3 bg-white hover:bg-gray-50 py-3 border border-gray-300 rounded-lg w-full font-medium text-gray-700"
           >
             
