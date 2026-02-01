9b9a273db81d269acecc8ebe7e14b1005bafb8fc
Mohammadreza <mellboybeatprod@gmail.com>
2026-02-01 08:41:04 +0330

    dont work JUST COMMIT

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,25 +3,27 @@ import { signIn } from 'next-auth/react'
 import React, { useState } from 'react'
 
 export default function LoginPage() {
-  // const [isLoading, setIsLoading] = useState(false)
+   const [isLoading, setIsLoading] = useState(false)
 
   const handleGoogleLogin = async () => {
-
+    setIsLoading(true)
     console.log("Google button clicked!") // این خط رو اضافه کن
     await signIn("google", { 
       callbackUrl: "/",
     })
 
+    setIsLoading(false)
 
   }
 
   const handleGitHubLogin = async () => {
-
+    setIsLoading(true)
 
     console.log("GitHub button clicked!") // این خط رو اضافه کن
     await signIn("github", { 
       callbackUrl: "/",
     })
+    setIsLoading(false)
 
   }
 
@@ -33,16 +35,19 @@ export default function LoginPage() {
           <button 
             onClick={handleGitHubLogin}
             
+            disabled={isLoading}
             className="bg-black hover:bg-gray-800 disabled:opacity-50 px-6 py-3 rounded-lg w-full text-white"
           >
-            Login with GitHub
+            {isLoading ? "Loading...":"Login with GitHub"}
           </button>
                     <button 
+                    disabled={isLoading}
             onClick={handleGoogleLogin}
             className="flex justify-center items-center gap-3 bg-white hover:bg-gray-50 py-3 border border-gray-300 rounded-lg w-full font-medium text-gray-700"
           >
             
-            Continue with Google
+            {isLoading ? "Loading...":"Continue with Google"}
+            
           </button>
         </form>
         <p className="mt-4 text-gray-600 text-sm text-center">
